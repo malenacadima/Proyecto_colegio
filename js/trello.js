01c5ba4e82970083482520d2ac5b5634ceaa21a3
meiphi <malena.cadima@gmail.com>
2020-10-17 19:29:26 -0300

    Subida de un proyecto que vine trabajando en la semana

---
+++ js/trello.js
@@ -0,0 +1,25 @@
+var carpeta = document.getElementById('clase_del_dia');
+var url='https://api.trello.com/1/members/me/boards?key=3c871d2858cd0d074c862049ffa8298a&token=24de60f808bd01f6a21feaccfe5c9a3bdc447e1a957b82949fe31b3ab6d4f2fa
+';
+var apiKey = '3c871d2858cd0d074c862049ffa8298a'
+
+var apiToken = '24de60f808bd01f6a21feaccfe5c9a3bdc447e1a957b82949fe31b3ab6d4f2fa'
+
+curl 'https://api.trello.com/1/boards/{idBoard}?key=3c871d2858cd0d074c862049ffa8298a&token=24de60f808bd01f6a21feaccfe5c9a3bdc447e1a957b82949fe31b3ab6d4f2fa'
+
+var tableros_all = 'https://api.trello.com/1/members/me/boards?fields=name,url&key=3c871d2858cd0d074c862049ffa8298a&token=24de60f808bd01f6a21feaccfe5c9a3bdc447e1a957b82949fe31b3ab6d4f2fa'
+
+var id_carpeta_general = '5eae38da9dc0505416d96022';
+
+var ni_puta_idea_data_carpeta_general = 'https://api.trello.com/1/boards/5eae38da9dc0505416d96022?key=3c871d2858cd0d074c862049ffa8298a&token=24de60f808bd01f6a21feaccfe5c9a3bdc447e1a957b82949fe31b3ab6d4f2fa';
+
+
+//tienen idlist iguales de la misma lista,
+var url_tarjetas_carpeta_general_IMPORTANTE = 'https://api.trello.com/1/boards/5eae38da9dc0505416d96022/cards'
+
+var listas_all = "https://api.trello.com/1/boards/5eae38da9dc0505416d96022/lists"
+
+var data_lista = https://api.trello.com/1/list/5eae38f4c9e9de136726447c/cards
+
+var imagenes_creo = https://api.trello.com/1/cards/5eb21ea10bfdc378499af422/attachments/5eb21f15b131f44fcf6bfffc
+https://api.trello.com/1/cards/IDCARDS/attachments/IDATTACHMENTS
